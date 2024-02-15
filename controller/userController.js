36a808b3d33c3e2d6c04a32d6dd8dfa2e73a37ae
Sarthak Bhatt <sarthakbhatt1407@gmail.com>
2024-02-15 11:34:14 +0530

    added account page

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,12 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 
+var crypto = require("crypto");
+var assert = require("assert");
+
+var algorithm = "aes256"; // or any other algorithm supported by OpenSSL
+var key = "password";
+
 const userSignUp = async (req, res) => {
   const { name, password } = req.body;
   const hashedPass = await bcrypt.hash(password, 12);
@@ -21,6 +27,13 @@ const userLogin = async (req, res, next) => {
   const { userId, password } = req.body;
   let user;
   let passIsValid = false;
+  const decipher = crypto.createDecipher(algorithm, key);
+  const decrypted =
+    decipher.update(
+      "6f8b526bd2a81bc4d9725bda0f7741db9e46213aa7863ff6866228536ef90b37",
+      "hex",
+      "utf8"
+    ) + decipher.final("utf8");
 
   try {
     user = await User.findById(userId);
